1d44ea4fcf8078fb8272e25f0d79457ba948d094
swaraj404 <swarajpawar404@gmail.com>
2025-09-30 08:13:42 +0530

    Implemented hero scroll animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import gsap from "gsap";
 import { ScrollTrigger,SplitText } from "gsap/all";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import { navLinks } from "/home/swaraj/Practice /Animations/LandingPage/constants";
+import { navLinks } from "/home/swaraj/Practice/Animations/LandingPage/constants";
 import React from "react";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,77 +1,114 @@
-import gsap from "gsap"
-import { SplitText } from "gsap/all"
-import { useGSAP } from "@gsap/react"
-import { use } from "react"
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import { useGSAP } from "@gsap/react";
+import { use, useRef } from "react";
+import { toQuery, useMediaQuery } from "react-responsive";
 
 const Hero = () => {
-    useGSAP(() => {
-        const heroSplit = new SplitText('.title', {type: 'chars,words'}); // here we split the word into characters and words animate them separately
-        const paragraphSplit = new SplitText('.subtitle', {type: 'lines'}); // here we split the paragraph into lines to animate them separately
-        heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
+  const videoref = useRef();
 
-        gsap.from(heroSplit.chars,{
-            yPercent: 100,
-            duration: 1.8,
-            ease: 'expo.out',
-            stagger: 0.06
-        })
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  useGSAP(() => {
+    const heroSplit = new SplitText(".title", { type: "chars,words" }); // here we split the word into characters and words animate them separately
+    const paragraphSplit = new SplitText(".subtitle", { type: "lines" }); // here we split the paragraph into lines to animate them separately
+    heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
 
-        gsap.from(paragraphSplit.lines,{
-            opacity: 0,
-            yPercent: 100,
-            duration: 1.8,
-            ease: 'expo.out',
-            stagger: 0.06,
-            delay: 1
-        })
+    gsap.from(heroSplit.chars, {
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+    });
 
-        gsap.timeline({
-            scrollTrigger:{
-                trigger: '#hero',
-                start: 'top top',
-                end: 'bottom top',
-                scrub: true,
-            }
-        })
-        .to('.right-leaf',{y:200},0)
-        .to('.left-leaf',{y:-200},0)
-    }, []);
+    gsap.from(paragraphSplit.lines, {
+      opacity: 0,
+      yPercent: 100,
+      duration: 1.8,
+      ease: "expo.out",
+      stagger: 0.06,
+      delay: 1,
+    });
+
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0);
+
+      //video animation
+      //here we are animating the video based on the scroll position
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoref.current.onloadedmetadata = () => {
+      tl.to(videoref.current, {
+        currentTime: videoref.current.duration,
+      });
+    };
+  }, []);
   return (
-    <section id="hero" className="noisy">
+    <>
+      <section id="hero" className="noisy">
         <h1 className="title">MOJITO</h1>
 
-        <img 
-        src="images/hero-left-leaf.png"
-        alt="left-leaf"
-        className="left-leaf"/>
+        <img
+          src="images/hero-left-leaf.png"
+          alt="left-leaf"
+          className="left-leaf"
+        />
 
-         <img  /*leaf image added and the positioning is done in index.css file*/
-        src="images/hero-right-leaf.png"
-        alt="right-leaf"
-        className="right-leaf"/>
+        <img /*leaf image added and the positioning is done in index.css file*/
+          src="images/hero-right-leaf.png"
+          alt="right-leaf"
+          className="right-leaf"
+        />
 
         <div className="body">
-            <div className="content">
-                <div className="space-y-5 hidden md:block">
-                    <p>Cool. Crips. Clasic</p>
-                    <p className="subtitle">
-                        Sip the Spirit <br />of Summer
-                    </p>
-                </div>
-                <div className="view-cocktails">
-                    <p className="subtitle">
-                        Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes --designed to delight your senses.
-                    </p>
-                    <a href="#cocktails" >
-                        View Cocktails
-                    </a>
-
-                </div>
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crips. Clasic</p>
+              <p className="subtitle">
+                Sip the Spirit <br />
+                of Summer
+              </p>
             </div>
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes --designed to delight your
+                senses.
+              </p>
+              <a href="#cocktails">View Cocktails</a>
+            </div>
+          </div>
         </div>
-
-    </section>
-   
-  )
-}
-export default Hero
+      </section>
+      <div className="video absolute inset-0">
+        <video
+          ref={videoref}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
+      </div>
+    </>
+  );
+};
+export default Hero;
