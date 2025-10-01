ecbbcd148c4d21f60dcae7d0d036461d5c4b692a
swaraj404 <swarajpawar404@gmail.com>
2025-10-01 12:43:45 +0530

    Implemented the art section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { navLinks } from "../constants";
 import React from "react";
 import Cocktails from "./components/Cocktails";
 import About from "./components/About";
+import Art from "./components/Art";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
     <Hero/>
     <Cocktails/>
     <About/>
+    <Art/>
    </main>
 
   );
