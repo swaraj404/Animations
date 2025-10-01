de14733458e60a20d945f7c6d449ee81a961ca13
swaraj404 <swarajpawar404@gmail.com>
2025-10-02 01:44:06 +0530

    working on menu section

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,41 +1,91 @@
 `use client`;
 import { useState } from "react";
-import { allCocktails } from "../../constants"
+import { allCocktails } from "../../constants";
 
-const Menu =() => {
-    const [currentIndex, setCurrentIndex] =  useState(0);
-    const totalCocktails = allCocktails.length;
+const Menu = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const totalCocktails = allCocktails.length;
 
-    const goToSlide = (index) => {
-        const newIndex= (index + totalCocktails) % totalCocktails;
-        setCurrentIndex(newIndex);
-    }
-    return (
-        <section id ="menu" aria-labelledby="menu-heading">
-            <img src="/images/slider-left-leaf.png" alt="left-leaf" id="m-left-leaf" />
-            <img src="/images/slider-right-leaf.png" alt="right-leaf" id="m-right-leaf" />
+  const goToSlide = (index) => {
+    const newIndex = (index + totalCocktails) % totalCocktails;
+    setCurrentIndex(newIndex);
+  };
+  const getCocktailAt = (indexOffset) => {
+    return allCocktails[
+      (currentIndex + indexOffset + totalCocktails) % totalCocktails
+    ];
+  };
 
-            <h2 id="menu-heading" className="sr-only">
-                Cocktail Menu
-            </h2>
+ const currentCocktail = getCocktailAt(0);
+ const prevCocktail = getCocktailAt(-1);
+ const nextCocktail = getCocktailAt(1);
+  return (
+    <section id="menu" aria-labelledby="menu-heading">
+      <img
+        src="/images/slider-left-leaf.png"
+        alt="left-leaf"
+        id="m-left-leaf"
+      />
+      <img
+        src="/images/slider-right-leaf.png"
+        alt="right-leaf"
+        id="m-right-leaf"
+      />
 
-             <nav className="cocktail-tabs" aria-label="Cocktail Navigation">
-		{allCocktails.map((cocktail, index) => {
-		 const isActive = index === currentIndex;
-		 
-		 return (
-			<button key={cocktail.id} className={`
-				${isActive
-				 ? 'text-white border-white'
-				 : 'text-white/50 border-white/50'}
-			 `}	onClick={() => goToSlide(index)}
-			>
-			 {cocktail.name}
-			</button>
-		 )
-		})}
-	 </nav>
-            
-        </section>
-    )}
-export default Menu
+      <h2 id="menu-heading" className="sr-only">
+        Cocktail Menu
+      </h2>
+
+      <nav className="cocktail-tabs" aria-label="Cocktail Navigation">
+        {allCocktails.map((cocktail, index) => {
+          const isActive = index === currentIndex;
+
+          return (
+            <button
+              key={cocktail.id}
+              className={`
+				${isActive ? "text-white border-white" : "text-white/50 border-white/50"}
+			 `}
+              onClick={() => goToSlide(index)}
+            >
+              {cocktail.name}
+            </button>
+          );
+        })}
+      </nav>
+
+      <div className="content">
+        <div className="arrows">
+          <button
+            className="text-left"
+            onClick={() => goToSlide(currentIndex - 1)}
+          >
+            <span>{prevCocktail.name}</span>
+            <img
+              src="/images/right-arrow.png"
+              alt="right-arrow"
+              aria-hidden="true"
+            />
+          </button>
+
+          <button
+            className="text-left"
+            onClick={() => goToSlide(currentIndex + 1)}
+          >
+            <span>{nextCocktail.name}</span>
+            <img
+              src="/images/left-arrow.png"
+              alt="left-arrow"
+              aria-hidden="true"
+            />
+          </button>
+        </div>
+
+        <div className="cocktail">
+          <img src={currentCocktail.image} className="object-contain" />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Menu;
