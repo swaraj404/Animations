2e3e2b627b9cb0e19265fbcb614390c888b5a7e2
swaraj404 <swarajpawar404@gmail.com>
2025-10-02 19:50:56 +0530

    optimization for mobile

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    target: 'es2015',
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-    // Remove problematic rollupOptions for now
-    chunkSizeWarningLimit: 1000,
-  },
-  optimizeDeps: {
-    include: ['gsap', '@gsap/react', 'react-responsive'],
-  },
-  server: {
-    host: true,
-  }
 })
